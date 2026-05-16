0f69f5254fabfdeea396c7da3d11ec6a82f44f4f
Keshav Patel <patelkeshav876@gmail.com>
2026-05-16 23:27:31 +0530

    secret key remove kiya

--- gen_session.py
+++ gen_session.py
@@ -2,7 +2,7 @@ from pyrogram import Client
 import asyncio
 
 async def gen():
-    async with Client("temp_session_gen", in_memory=True, api_id=31942889, api_hash="6e84cb671c7241f0dc73fb55fd00ec7d") as c:
+    async with Client("temp_session_gen", in_memory=True, api_id=*, api_hash="*") as c:
         print(await c.export_session_string())
 
 asyncio.run(gen())
